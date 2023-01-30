b2136b87760dcf7fbdfcabfebeb218efb46c2a01
rafroes1 <rafafroes@outlook.com>
2023-01-30 10:09:47 -0300

    Initial Commit

---
+++ FructusApp/FructusAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusAppApp.swift
+//  FructusApp
+//
+//  Created by Rafael Carvalho on 30/01/23.
+//
+
+import SwiftUI
+
+@main
+struct FructusAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
